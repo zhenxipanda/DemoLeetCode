b2f9c56a66c4b2b0680adeacc3087b8f08320399
zhenxipanda <1359619070@qq.com>
2020-09-30 10:28:06 +0800

    create lc 149

--- src/LeetCode/MaxPointsOnALine149.java
+++ src/LeetCode/MaxPointsOnALine149.java
@@ -1,10 +1,38 @@
 package LeetCode;
+/**
+ * LeetCode 149
+ * 题意：给定一个二维平面，平面上有 n 个点，求最多有多少个点在同一条直线上。
+ * 示例 1:
+ * 输入: [[1,1],[2,2],[3,3]]
+ * 输出: 3
+ * 解释:
+ * ^
+ * |
+ * |        o
+ * |     o
+ * |  o  
+ * +------------->
+ * 0  1  2  3  4
+ * 示例 2:
+ * 输入: [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+ * 输出: 4
+ * 解释:
+ * ^
+ * |
+ * |  o
+ * |     o        o
+ * |        o
+ * |  o        o
+ * +------------------->
+ * 0  1  2  3  4  5  6
+ * 思路：双层循环，外层一个map，记录与当前点构成的直线情况，斜率使用最大公约数表示，
+ *      因为可能有重复点，所以需要考虑将重复点统计进去，
+ *      40.53% , 45.07%
+ */
 
 import java.util.HashMap;
-/* LeetCode 149
- * 思路：两层循环，探寻过每个点的直线，记录这些直线上最多点的数量
- *
- * */
+import java.util.Map;
+
 public class MaxPointsOnALine149 {
 
     public static void main(String[] args) {
@@ -15,40 +43,29 @@ public class MaxPointsOnALine149 {
     }
 
     public static int maxPoints(int[][] points) {
-        if (points.length <= 2) {
+        if(points.length < 3){
             return points.length;
         }
-        int max = 0;
-        int samePoint = 0;
-        for (int i = 0; i < points.length; i++) {
-            HashMap<String, Integer> map = new HashMap<>();
-            samePoint = 0;
-            for (int j = 0; j < points.length; j++) {
-                if (i == j) {
+        int res = 0;
+        for(int i = 0 ; i < points.length ; i ++){
+            int samePoint = 0;
+            int max = 0;
+            Map<String , Integer> map = new HashMap<>();
+            for(int j = i + 1 ; j < points.length ; j ++){
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+                if(x == 0 && y == 0){
+                    samePoint ++ ;
                     continue;
                 }
-                if (points[i][0] == points[j][0] && points[i][1] == points[j][1]) {
-                    samePoint++;
-                    continue;
-                }
-                int dx = points[j][0] - points[i][0];
-                int dy = points[j][1] - points[i][1];
-                int d = gcd(dx, dy);
-                String temp = Integer.toString(dx / d) + "#" + Integer.toString(dy / d);
-                if (map.get(temp) == null) {
-                    map.put(temp, 2);
-                } else {
-                    int index = map.get(temp) + 1;
-                    map.put(temp, index);
-                }
+                int d = gcd(x , y);
+                String key = (x / d) + "#" + (y / d);
+                map.put(key , map.getOrDefault(key , 0) + 1);
+                max = Math.max(max , map.get(key));
             }
-            for (int a : map.values()) {
-                if (a + samePoint > max) {
-                    max = a + samePoint;
-                }
-            }
+            res = Math.max(res , max + samePoint + 1);
         }
-        return max == 0 ? samePoint + 1 : max;
+        return res;
     }
 
     public static int gcd(int a, int b) {
