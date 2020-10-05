eff5a7c3329c8447161df33a4e2c271e8b0d1866
zhenxipanda <1359619070@qq.com>
2020-10-05 10:33:56 +0800

    add lc 164-171

--- src/LeetCode/MajorityElement169.java
+++ src/LeetCode/MajorityElement169.java
@@ -1,5 +1,19 @@
 package LeetCode;
 
+/**
+ * LeetCode 169
+ * 题意：给定一个大小为 n 的数组，找到其中的多数元素。
+ * 多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ * 示例 1:
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ * 思路：O(N)的时间复杂度就能完成，相邻元素做比较，如果相等，time ++
+ *      否则,time -- ，如果 time == 0 , major = nums[i], time ++ ;
+ */
 public class MajorityElement169 {
     public static void main(String[] args) {
         int[] nums = {3,2,2,3,2,3,3};
