7a6bbe19c01ccd35163d681b4f016608f84ab760
zhenxipanda <1359619070@qq.com>
2020-10-23 14:59:44 +0800

    create offer 58-2

---
+++ src/JianZhiOffer/interview58_2.java
@@ -0,0 +1,29 @@
+package JianZhiOffer;
+
+/**
+ * 剑指offer 58-2
+ * 题意：字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+ * 请定义一个函数实现字符串左旋转操作的功能。
+ * 比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ * 示例 1：
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ * 限制：
+ * 1 <= k < s.length <= 10000
+ * 思路：直接使用substring()函数
+ *      100% , 93.43%
+ */
+public class interview58_2 {
+    public static void main(String[] args) {
+        String s = "hello";
+        int n = 2;
+        String res = reverseLeftWords(s, n);
+        System.out.println(res);
+    }
+    public static String reverseLeftWords(String s, int n) {
+        return s.substring(n) + s.substring(0 , n);
+    }
+}
